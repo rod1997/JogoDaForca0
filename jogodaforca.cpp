2686d93cfbab10d8ae0ac5a4c87348d2e9d0b4e2
rodrigo diego doro <rdieguez750@gmail.com>
2020-11-29 22:31:06 -0300

    adicionado o arquivo jogodaforca.cpp

---
+++ jogodaforca.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+   std::cout << "--------------------------------------------" << std::endl;
+   std::cout << "|                                          |" << std::endl;
+   std::cout << "|               JOGO DA FORCA              |" << std::endl;
+   std::cout << "|                                          |" << std::endl;
+   std::cout << "--------------------------------------------" << std::endl;
+
+
+
+
+
+
+
+
+
+   return 0;
+}
